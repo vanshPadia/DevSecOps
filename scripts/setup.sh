72388a6bd46f41c13dbc13095f9c797937545b9e
vanshPadia <vansh.padia19@gmail.com>
2026-05-25 16:00:03 +0530

    checking new code

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,6 +5,6 @@ APP_TYPE=$1
 echo "Setting up runtime for: $APP_TYPE"
 
 if [ "$APP_TYPE" == "python" ]; then
-    apt update
-    apt install -y python3 python3-pip
+    python3 --version
+    pip3 --version
 fi
